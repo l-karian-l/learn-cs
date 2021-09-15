835e8d8882c03e9019aa37d722fb2a632b79e36e
Анастасия <@LAPTOP-AHROEA22>
2021-09-15 19:33:30 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
